4e1dd5ae66f867697f1d85e9d7b9051e89e50b5d
jeasterman <jonathaneasterman@gmail.com>
2022-01-14 19:29:01 +0000

    Refactor huxlxy weights/rarities logic into module

--- packages/huxlxy-airdrop/src/index.ts
+++ packages/huxlxy-airdrop/src/index.ts
@@ -1,26 +1,5 @@
-import fs from "fs";
-import path from "path";
-import { layers, rarities, weights } from "./constants";
+import { AirdropType, generateSnapshot } from "../../lib/snapshot";
 
 (async () => {
-  layers.forEach((layer) => {
-    rarities.forEach((rarity) => {
-      const oldPathBase = path.join(__dirname, "../layers", layer, rarity);
-      const files = fs.readdirSync(oldPathBase);
-      const weight = Math.round(weights[rarity] / files.length);
-      files.forEach((file) => {
-        const oldPath = path.join(__dirname, "../layers", layer, rarity, file);
-        const parsedFile = path.parse(oldPath);
-        const ext = parsedFile.ext;
-        const name = parsedFile.name;
-        const newPathBase = path.join(__dirname, `../layers-new/${layer}`);
-        const newPath = path.join(newPathBase, `${name}#${weight}${ext}`);
-        try {
-          fs.mkdirSync(newPathBase);
-        } catch (e) {}
-        console.log(oldPath, "==>", newPath);
-        fs.copyFileSync(oldPath, newPath);
-      });
-    });
-  });
+  await generateSnapshot(AirdropType.Huxlxy);
 })();
