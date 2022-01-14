4d149dcb74cd6d5a81ce557822fb8c399b264a4e
jeasterman <jonathaneasterman@gmail.com>
2022-01-14 19:31:47 +0000

    Add airdrop logic and Ownable to HuxlxyNFT

--- packages/huxlxy-contracts/src/HuxlxyNFT.sol
+++ packages/huxlxy-contracts/src/HuxlxyNFT.sol
@@ -2,20 +2,52 @@
 pragma solidity ^0.8.10;
 
 import "./ERC721A.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 
-error DoesNotExist();
-
-contract HuxlxyNFT is ERC721A {
+contract HuxlxyNFT is ERC721A, Ownable {
   string public baseURI;
 
-  constructor() ERC721A("Huxlxy", "HUXLXY", 1050) {}
+  bytes32 public immutable merkleRoot;
+  uint256 private immutable airdropStart;
+  mapping(address => bool) private claimed;
 
-  function mint(uint16 amount) external {
-    _safeMint(msg.sender, amount);
+  constructor(bytes32 merkleRoot_) ERC721A("Huxlxy", "HUXLXY", 850) {
+    merkleRoot = merkleRoot_;
+    airdropStart = block.timestamp;
   }
 
-  function _baseURI() internal view virtual returns (string memory) {
+  function isClaimed(address user) public view returns (bool) {
+    return claimed[user];
+  }
+
+  function mint(
+    address account,
+    uint256 amount,
+    bytes32[] calldata merkleProof
+  ) external {
+    // Check merkle proof
+    require(!isClaimed(account), "Airdrop already claimed.");
+    require(totalSupply() < 10000, "Mint limit reached.");
+    bytes32 node = keccak256(abi.encodePacked(account, amount));
+
+    require(
+      MerkleProof.verify(merkleProof, merkleRoot, node),
+      "Invalid proof."
+    );
+
+    claimed[account] = true;
+
+    _safeMint(account, amount);
+  }
+
+  function _baseURI() internal view override returns (string memory) {
     // TODO: Change
     return "";
+  }
+
+  function sweep() public onlyOwner {
+    require(block.timestamp > airdropStart + 30 days, "Airdrop period has not ended.");
+    _safeMint(msg.sender, 10000 - totalSupply());
   }
 }
