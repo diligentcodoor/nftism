b4f6ed60466032abb956d7a32e6185d727ccc3e2
jeasterman <jonathaneasterman@gmail.com>
2022-02-01 13:33:47 +0000

    Update snapshot

--- packages/huxlxy-airdrop/src/index.ts
+++ packages/huxlxy-airdrop/src/index.ts
@@ -3,7 +3,7 @@ import { generateMerkleTree } from "../../lib/src/merkle";
 import { AirdropType } from "../../lib/src/types";
 
 (async () => {
-  await generateSnapshot(AirdropType.Huxlxy, 14107438);
+  await generateSnapshot(AirdropType.Huxlxy, 14119983);
   await generateBalances(AirdropType.Huxlxy);
   const tree = generateMerkleTree(AirdropType.Huxlxy);
   console.log(`Merkle Root: ${tree.getHexRoot()}`);
