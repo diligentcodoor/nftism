9f5981a3281c53351adf253ccc1db700e6c0e3c3
jeasterman <jonathaneasterman@gmail.com>
2022-02-07 11:54:01 +0000

    Add minimal progress bar for client side routing

--- packages/nftism-app/pages/_app.tsx
+++ packages/nftism-app/pages/_app.tsx
@@ -1,18 +1,25 @@
 import type { AppProps } from "next/app";
+import dynamic from "next/dynamic";
 import { ChakraProvider } from "@chakra-ui/react";
 
 import WagmiProvider from "@lib/hooks/wagmi-provider";
-import customTheme from "@styles/theme";
 
+const ProgressBar = dynamic(() => import("@components/ui/ProgressBar"), {
+  ssr: false,
+});
+import customTheme from "@styles/theme";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={customTheme}>
-      <WagmiProvider>
-        <Component {...pageProps} />
-      </WagmiProvider>
-    </ChakraProvider>
+    <>
+      <ChakraProvider theme={customTheme}>
+        <WagmiProvider>
+          <Component {...pageProps} />
+        </WagmiProvider>
+      </ChakraProvider>
+      <ProgressBar />
+    </>
   );
 }
 
